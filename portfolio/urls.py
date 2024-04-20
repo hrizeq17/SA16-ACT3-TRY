50925dc97a17aad2fb0bc452301284267e0fd181
hrizeq17 <hrizeq@memphis.edu>
2024-04-20 15:46:32 -0500

    s

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('work/', views.work, name='work'),
     path('contact/', views.contact, name='contact'),
